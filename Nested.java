ad9f8229514a7074ce9727a552e2f48ae544d1fc
VISHWAK <vishwaksen2212@gmail.com>
2025-01-06 09:47:56 +0530

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[]args) throws InterruptedException
+    {
+        int a =2;
+        for (int i=1; i<=6 ; i++ )
+        {
+            for (int j=1; j<=6 ; j++ )
+            {
+                a=a+2;
+            }
+        }
+        System.out.println(a);
+    }
+    
+}
